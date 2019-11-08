0684b3ed693a424d964ac0e5b2267f31e2003969
Xavier Goffin <xavier.goffin@stack-labs.com>
2019-11-08 11:04:50 +0100

    [mission1] fixed bucket name in briefing
    Former-commit-id: cd7dc601701674fbff938e6fa15f82d1ffa99d4f

--- mission1/mission1-deploy.sh
+++ mission1/mission1-deploy.sh
@@ -33,7 +33,7 @@ aws --profile $SECGAME_USER_PROFILE s3 mb s3://evilcorp-evilbucket-$SECGAME_USER
 echo "[AWS-Secgame] Bucket created"
 
 #Imports resources from the original mission1 folder, and copies them onto the bucket.
-aws --profile $SECGAME_USER_PROFILE s3 --quiet cp ./../mission1/resources/evilcorp-evilbucket-data s3://evilcorp-evilbucket-$SECGAME_USER_ID --recursive
+aws --profile $SECGAME_USER_PROFILE s3 --quiet cp ./resources/evilcorp-evilbucket-data s3://evilcorp-evilbucket-$SECGAME_USER_ID --recursive
 echo "[AWS-Secgame] Ressources copied on bucket"
 
 #Sets the bucket's ACL (access control list) to public for maximum vulnerability
@@ -51,6 +51,6 @@ touch briefing.txt
 echo "Greetings, agent. Our company received strange reports regarding another large-scale, multinational company called EvilCorp \
 Word has it that they are plotting something. Something bad. However, our informants have found a lead. \
 Our guy working at Amazon Web Services reported seeing the name of Evilcorp passing by in an S3 report. There may be something to see. \
-Look up their evilcorp-evilbucket-$SECGAME_USER_PROFILE bucket. There should be interesting files on there, and a hint regarding their plans..." >> briefing.txt
+Look up their evilcorp-evilbucket-$SECGAME_USER_ID bucket. There should be interesting files on there, and a hint regarding their plans..." >> briefing.txt
 
 echo "[AWS-Secgame] Mission 1 deployment complete. Mission folder is ./mission1-$SECGAME_USER_ID. Read the briefing.txt file to begin."
