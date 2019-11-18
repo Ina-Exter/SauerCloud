f3abeb7f41e1c8034c1319c3b1832dce8ab6a378
Xavier Goffin <xavier.goffin@stack-labs.com>
2019-11-18 15:25:03 +0100

    [mission1] ported on terraform
    Former-commit-id: c477208c48eba016d97cbeb6d51c0fdd0f8c5080

--- mission1/mission1-deploy.sh
+++ mission1/mission1-deploy.sh
@@ -11,78 +11,55 @@ echo "[AWS-Secgame] User IP: $USER_IP"
 
 #ALWAYS assume that this script will run in the mission folder mission1-user_id
 
-#Request consent, deploy only if given
-echo "[AWS-Secgame] This will setup the first mission's bucket and associated resources. Is this acceptable? (yes/no)"
+#Git data is zipped. Unzip it before starting terraform
+echo "[AWS-Secgame] Unzipping data."
+cd resources
+unzip -qq evilcorp-evilbucket-data.zip
+
+#Initialize terraform
+cd terraform
+echo "[AWS-Secgame] Initializing terraform."
+terraform init
+
+#Pass the required variables (profile, region?, id, key) to terraform and plan
+terraform plan -var="profile=$SECGAME_USER_PROFILE" -var="id=$SECGAME_USER_ID"
+
+#IF AND ONLY IF user consents, deploy
+echo "[AWS-Secgame] Is this setup acceptable? (yes/no)"
 echo "[AWS-Secgame] Only \"yes\" will be accepted as confirmation."
 read answer
 if [[ ! $answer == "yes" ]]
 then
 	echo "[AWS-Secgame] Abort requested. Destroying target folder."
-	cd ..
+	cd ../../..
 	#If trash doesn't exist, make it
 	if [[ ! -d "trash" ]]
 	then
-	       	mkdir trash
+        	mkdir trash
 	fi
-	mv mission1-$SECGAME_USER_ID ./trash/
+	mv ./mission1-$SECGAME_USER_ID ./trash/
 	exit 2
 fi
 
-#This first command makes a new bucket called evilcorp-evilbucket-user_id.
-aws --profile $SECGAME_USER_PROFILE s3 mb s3://evilcorp-evilbucket-$SECGAME_USER_ID
-if [[ ! $? == 0 ]]
+#DEPLOYYYYYYYYYYYYYYYYYYYY
+terraform apply -auto-approve -var="profile=$SECGAME_USER_PROFILE" -var="id=$SECGAME_USER_ID"
+
+#check terraform apply's return code, act depending on it. 0 is for a flawless execution, 1 means an error has arisen
+if [[ $? != 0 ]]
 then
-	echo "[AWS-Secgame] Non-zero return code on operation. Abort."
-	cd ..
-	#No resource has been created, just delete the folder
+	echo "[AWS-Secgame] Non-zero return code on terraform apply. Rolling back."
+	terraform destroy -auto-approve -var="profile=$SECGAME_USER_PROFILE" -var="id=$SECGAME_USER_ID"
+	cd ../../..
+	#If trash doesn't exist, make it
 	if [[ ! -d "trash" ]]
 	then
-	       	mkdir trash
+        	mkdir trash
 	fi
-	mv mission1-$SECGAME_USER_ID ./trash/
+	mv ./mission1-$SECGAME_USER_ID ./trash/
 	exit 2
 fi
-echo "[AWS-Secgame] Bucket created"
 
-#Git data is zipped. Unzip it before the transfer
-cd resources
-unzip -qq evilcorp-evilbucket-data.zip
-cd ..
-
-#Copy the files onto the bucket.
-aws --profile $SECGAME_USER_PROFILE s3 --quiet cp ./resources/evilcorp-evilbucket-data s3://evilcorp-evilbucket-$SECGAME_USER_ID --recursive
-if [[ ! $? == 0 ]]
-then
-	echo "[AWS-Secgame] Non-zero return code on operation. Abort."
-	#If this fails, then the delete script has to be ran...
-	source ./mission1-destroy.sh --abort
-	exit 2
-fi
-echo "[AWS-Secgame] Ressources copied on bucket"
-
-#Sets the bucket's ACL (access control list) to public for maximum vulnerability
-aws --profile $SECGAME_USER_PROFILE s3api put-bucket-acl --acl public-read --bucket evilcorp-evilbucket-$SECGAME_USER_ID
-if [[ ! $? == 0 ]]
-then
-	echo "[AWS-Secgame] Non-zero return code on operation. Abort."
-	#If this fails, then the delete script has to be ran...
-	source ./mission1-destroy.sh --abort
-	exit 2
-fi
-echo "[AWS-Secgame] Bucket ACL set"
-
-#This gets a bit tacky. All files also have to have public access, and I found no better way than to pipe it all together and pray.
-#This essentially lists bucket content, then for each, sets ACL as public-read. Since .git folders a a bit large, this may take up to 30 seconds.
-echo "[AWS-Secgame] Setting file ACL, please wait (expected wait under 1 minute)."
-aws --profile $SECGAME_USER_PROFILE s3 ls s3://evilcorp-evilbucket-$SECGAME_USER_ID --recursive|awk '{cmd="aws --profile $SECGAME_USER_PROFILE s3api put-object-acl --acl public-read --bucket evilcorp-evilbucket-$SECGAME_USER_ID --key "$4; system(cmd)}'
-if [[ ! $? == 0 ]]
-then
-	echo "[AWS-Secgame] Non-zero return code on operation. Abort."
-	#If this fails, then the delete script has to be ran...
-	source ./mission1-destroy.sh --abort
-	exit 2
-fi
-echo "[AWS-Secgame] File ACL set"
+cd ../..
 
 # A briefing serves as a starting point for the user, now with extra hacky flavour!
 touch briefing.txt
--- mission1/mission1-destroy.sh
+++ mission1/mission1-destroy.sh
@@ -8,36 +8,22 @@ echo "[AWS-Secgame] User IP: $USER_IP"
 
 #ALWAYS assume this will run from the mission dir!
 
-#This option checks that the script did not start with --abort, which skips user confirmation and focuses on resource destruction
-if [[ $1 == "--abort" ]]
-then
-	#Bucket destruction
-	aws --profile $SECGAME_USER_PROFILE s3 rb s3://evilcorp-evilbucket-$SECGAME_USER_ID --force
-	cd ..
-	if [[ ! -d "trash" ]]
-	then
-	       	mkdir trash
-	fi
-	mv mission1-$SECGAME_USER_ID ./trash/
-	exit 7
-fi
-
-#Request consent, restore if not given
-echo "[AWS-Secgame] This will destroy all mission1-associated resources. Is this acceptable? (yes/no)"
+#Request confirmation
+echo "[AWS-Secgame] Destroy mission1-$SECGAME_USER_ID? (yes/no)"
 echo "[AWS-Secgame] Only \"yes\" will be accepted as confirmation."
 read answer
 if [[ ! $answer == "yes" ]]
 then
-	echo "Abort requested. Restoring target folder."
-	cd ../..
-	mv ./trash/mission1-$SECGAME_USER_ID .
+	echo "[AWS-Secgame] Abort requested. Restoring target folder."
+	cd ../../
+	mv ./trash/mission1-$SECGAME_USER_ID ./
 	exit 2
 fi
 
-
-#Destroy the bucket based on user ID. User ID should be passed by start.sh
-echo "[AWS-Secgame] Force-destroying bucket"
-aws --profile $SECGAME_USER_PROFILE s3 rb s3://evilcorp-evilbucket-$SECGAME_USER_ID --force
+#destroy terraform
+echo "[AWS-Secgame] Destroying terraform resources"
+cd resources/terraform
+terraform destroy -auto-approve -var="profile=$SECGAME_USER_PROFILE"
 
 echo "[AWS-Secgame] Mission 1 destroy complete."
 exit 0
